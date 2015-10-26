b8678339c12c9d29118e58a58e2b4feaa7ca8a8c
drawde <drawde@iMac-de-Web.local>
2015-10-26 15:17:34 -0500

    responsive

--- header.php
+++ header.php
@@ -19,25 +19,29 @@
 </div>
 <div class="menu-mobile hidden">
     <ul>
-        <li><a href="index.php">INICIO</a><p class="close">X</p></li>
         <li>
-            <a class="a-one" href="servicios.php">SERVICIOS DE BANCA</a>
-            <ul class="menu-one">
-                <li><a href="financiacion.php">FINANCIACIÓN</a></li>
-                <li><a href="valorizacion.php">FUSIONES Y ADQUISICIONES</a></li>
-                <li><a href="infraestructura.php">INFRAESTRUCTRURA</a></li>
-                <li><a href="">MERCADO DE CAPITALES</a></li>
+            <a href="nosotros.php">QUIENES SOMOS</a>
+            <ul>
+                <li><a href="nosotros.php">HISTORIA</a></li>
+                <li><a href="equipo.php">EQUIPO DE TRABAJO</a></li>
             </ul>
         </li>
         <li>
-            <a class="a-two"  href="experiencias.php">EXPERIENCIAS</a>
-            <ul class="menu-two">
-                <li><a href="real.php">SECTOR REAL</a></li>
-                <li><a href="infraestructuras.php">INFRAESTRUCTURA</a></li>
-                <li><a href="inmobiliaria.php">INMOBILIARIA</a></li>
-                <li><a href="agricola.php">AGRÍCOLA</a></li>
+            <a href="financiacion.php">FINANCIACIÓN</a>
+
+        </li>
+        <li>
+            <a class="a-one" href="mya.php"> FUSIONES & ADQUISICIONES </a>
+        </li>
+        <li>
+            <a href="infraestructura.php">ESTRUCTURACIÓN</a>
+            <ul>
+                <li><a href="infraestructura.php">INFRAESTRUCTURA</a></li>
+                <li><a HREF="inmobiliaria.php">INMOBILIARIA</a></li>
             </ul>
         </li>
+
+        <li><a href="fondo.php">FONDO DE INVERSIÓN INMOBILIARIO</a></li>
         <li>
             <a class="a-three" href="clienes.php">CLIENTES</a>
             <ul class="menu-three">
@@ -45,7 +49,7 @@
                 <li><a href="internacionales.php">CLIENTES INTERNACIONALES</a></li>
             </ul>
         </li>
-        <li><a href="nosotros.php">QUIENES SOMOS</a></li>
+
         <li><a href="contacto.php">CONTÁCTENOS</a></li>
     </ul>
 </div>
