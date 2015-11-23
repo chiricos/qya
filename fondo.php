1eec3def4c0093e26aac00b23efeda0d0d102e8b
drawde <drawde@iMac-de-Web.local>
2015-11-23 18:49:09 -0500

    terminado qya

--- fondo.php
+++ fondo.php
@@ -32,4 +32,41 @@
 
 
 
-<?php include('footer.php');
+<footer style="position: absolute;bottom: 0">
+    <ul>
+        <li>
+            <a href="nosotros.php">QUIENES SOMOS</a>
+            <ul>
+                <li><a href="nosotros.php">HISTORIA</a></li>
+                <li><a href="equipo.php">EQUIPO DE TRABAJO</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="financiacion.php">FINANCIACIÓN</a>
+
+        </li>
+        <li>
+            <a class="a-one" href="mya.php"> FUSIONES & ADQUISICIONES </a>
+        </li>
+        <li>
+            <a href="infraestructura.php">ESTRUCTURACIÓN</a>
+            <ul>
+                <li><a href="infraestructura.php">INFRAESTRUCTURA</a></li>
+                <li><a HREF="inmobiliaria.php">INMOBILIARIA</a></li>
+            </ul>
+        </li>
+
+        <li><a href="fondo.php">FONDO DE INVERSIÓN INMOBILIARIO</a></li>
+        <li>
+            <a class="a-three" href="clienes.php">CLIENTES</a>
+            <ul class="menu-three">
+                <li><a href="clienes.php">CLIENTES NACIONALES</a></li>
+                <li><a href="internacionales.php">CLIENTES INTERNACIONALES</a></li>
+            </ul>
+        </li>
+
+        <li><a href="contacto.php">CONTÁCTENOS</a></li>
+    </ul>
+</footer>
+</body>
+</html>
--- footer.php
+++ footer.php
@@ -1,12 +1,28 @@
 <footer>
-    <ul style="height: 100px">
-        <!--<li>
+    <ul>
+        <li>
+            <a href="nosotros.php">QUIENES SOMOS</a>
+            <ul>
+                <li><a href="nosotros.php">HISTORIA</a></li>
+                <li><a href="equipo.php">EQUIPO DE TRABAJO</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="financiacion.php">FINANCIACIÓN</a>
+
+        </li>
+        <li>
+            <a class="a-one" href="mya.php"> FUSIONES & ADQUISICIONES </a>
+        </li>
+        <li>
             <a href="infraestructura.php">ESTRUCTURACIÓN</a>
             <ul>
                 <li><a href="infraestructura.php">INFRAESTRUCTURA</a></li>
                 <li><a HREF="inmobiliaria.php">INMOBILIARIA</a></li>
             </ul>
         </li>
+
+        <li><a href="fondo.php">FONDO DE INVERSIÓN INMOBILIARIO</a></li>
         <li>
             <a class="a-three" href="clienes.php">CLIENTES</a>
             <ul class="menu-three">
@@ -14,20 +30,8 @@
                 <li><a href="internacionales.php">CLIENTES INTERNACIONALES</a></li>
             </ul>
         </li>
-        <li><a href="nosotros.php">QUIENES SOMOS</a></li>
-        <li>
-            <a href="financiacion.php">FINANCIACIÓN</a>
 
-        </li>
-        <li>
-            <a class="a-one" href="mya.php"> M & A </a>
-        </li>
-
-
-        <li><a href="fondo.php">FONDO DE INVERSIÓN INMOBILIARIO</a></li>
-
-
-        <li><a href="contacto.php">CONTÁCTENOS</a></li>-->
+        <li><a href="contacto.php">CONTÁCTENOS</a></li>
     </ul>
 </footer>
 </body>
